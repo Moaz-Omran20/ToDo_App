dee0f2ff0c640a1f0dfe7a72f8206c361bde51ae
Moaz <moazomran20@gmail.com>
2023-05-16 21:05:48 +0300

    adding task items

--- lib/screens/tasks_tab.dart
+++ lib/screens/tasks_tab.dart
@@ -1,10 +1,43 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/widgets/task_item.dart';
 
-class TasksTab extends StatelessWidget {
+class TasksTab extends StatefulWidget {
   const TasksTab({Key? key}) : super(key: key);
 
   @override
+  State<TasksTab> createState() => _TasksTabState();
+}
+
+DateTime selectDate = DateTime.now();
+
+class _TasksTabState extends State<TasksTab> {
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+    return Column(
+      children: [
+        DatePicker(
+          DateTime.now(),
+          height: 100,
+          initialSelectedDate: DateTime.now(),
+          selectionColor: Theme.of(context).primaryColor,
+          selectedTextColor: Colors.white,
+          onDateChange: (date) {
+            // New date selected
+            setState(() {
+              selectDate = date;
+            });
+          },
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return TaskItem();
+            },
+            itemCount: 10,
+          ),
+        )
+      ],
+    );
   }
 }
