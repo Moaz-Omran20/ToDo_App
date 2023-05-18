fdd0533887c62b078b6e28d42d77e3f008dc0e34
Moaz <moazomran20@gmail.com>
2023-05-18 14:50:57 +0300

    integration with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/styles/app_themes.dart';
 import 'package:todo_app/home_layout/home_layout.dart';
 import 'package:todo_app/splash_screen/splash.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
--- lib/screens/widgets/task_item.dart
+++ lib/screens/widgets/task_item.dart
@@ -7,49 +7,52 @@ class TaskItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: 5,
-                height: 100,
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Task Title",style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 20),),
-                Text(
-                  "Task description",
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(fontSize: 14),
+      child: InkWell(
+        child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: 5,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 5,
+                  height: 100,
+                  color: Colors.blue,
                 ),
-              ],
-            ),
-            Spacer(),
-            Container(
-              margin: EdgeInsets.only(right: 20),
-              width: 50,
-              height: 30,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              child: Icon(
-                Icons.done,
-                color: Colors.white,
               ),
-            ),
-          ],
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Task Title",style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 20),),
+                  Text(
+                    "Task description",
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayLarge!
+                        .copyWith(fontSize: 14),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Container(
+                margin: EdgeInsets.only(right: 20),
+                width: 50,
+                height: 30,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Icon(
+                  Icons.done,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
